2c034bd2f37d48d1987ea6a931341d53a834d78d
nanwani-karan <nanwanik21@gmail.com>
2025-05-22 07:26:38 +0000

    datatypesummary

---
+++ 01_basics/test.js
@@ -0,0 +1,12 @@
+let myyoutubename = "hiteshchoudharydotcom"
+let anothername = myyoutubename
+anothername = "chaiaurcode"
+console.log(anothername);
+let userone ={
+    email: "user@google.com",
+    upi:"user@ybl"
+}
+let usertwo = userone
+usertwo.email ="hitesh@gmail.com"
+console.log(userone.email);
+console.log(usertwo.email);
